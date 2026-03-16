d6b84fe392ca2a6c2ba162fbc327eab87226f431
Kristofor Jones <kris.jones@live.co.uk>
2026-03-16 10:50:49 +0000

    backend/frontend integration
    updating to use websocket fully after testing locally

--- homePage.js
+++ homePage.js
@@ -1,4 +1,4 @@
-const machines = ['Machine A', 'Machine B', 'Machine C', 'Machine D'];
+const machines = ['Picker', 'Grabber', 'Furnace', 'Smelter'];
 const tbody = document.getElementById('tbody');
 
 const dummystates = ['idle', 'starved', 'producing']
@@ -13,25 +13,27 @@ for (const machine of machines) {
 }
 
 
-setInterval(() => {
-  const from = machines[Math.floor(Math.random() * machines.length)];
-  const state = dummystates[Math.floor(Math.random() * dummystates.length)];
-  handleUpdate({ from, state });
-}, 1000);
+//setInterval(() => {
+//  const from = machines[Math.floor(Math.random() * machines.length)];
+//  const state = dummystates[Math.floor(Math.random() * dummystates.length)];
+//  handleUpdate({ from, state });
+//}, 1000);
 
-//const ws = new WebSocket('wss://your-url');
+const ws = new WebSocket('https://observerdashboard.onrender.com');
 
-//ws.onmessage = (event) => {
- // const { from, state } = JSON.parse(event.data);
- // const cell = document.getElementById(from);
- // cell.textContent = state;
-//};
-
-function handleUpdate({ from, state }) {
+ws.onmessage = (event) => {
+  console.log(event)
+  const { from, state } = JSON.parse(event.data);
+  console.log(state)
   const cell = document.getElementById(from);
-  if (cell){
-    cell.textContent = state;
-    cell.className = state;
-  } 
+  cell.textContent = state;
+  cell.className = state;
+};
 
-}
+//function handleUpdate({ from, state }) {
+ // const cell = document.getElementById(from);
+  //if (cell){
+  //  cell.textContent = state;
+   // cell.className = state;
+  //} 
+//}
